2cddbc44abe03be083bd944dcd1f014f4f8d7ae7
willjcim <will@aptlabs.net>
2025-12-03 22:48:39 +0000

    day 3 - modifies p2 to also work for p1

--- 3/2.py
+++ 3/2.py
@@ -1,4 +1,4 @@
-def get_largest_joltage(file_name: str):
+def get_largest_joltage(file_name: str, cell_count: int):
     # var declaration
     total_joltage = 0
 
@@ -7,13 +7,13 @@ def get_largest_joltage(file_name: str):
         for battery_bank in batteries:
             battery_bank_list = list(battery_bank.strip('\n'))
 
-            # start with just the first 12 battery cells in the bank 
-            strongest_cells = battery_bank_list[:12]
+            # start with just the first n battery cells in the bank 
+            strongest_cells = battery_bank_list[:cell_count]
 
             # go through all of the cells in the battery bank
-            for cell in range(12, len(battery_bank_list), 1): 
+            for cell in range(cell_count, len(battery_bank_list), 1): 
                 # go through each of the cells in the strongest cells list 
-                for strong_cell in range(0, 12, 1):
+                for strong_cell in range(0, cell_count, 1):
                     # test with each item in the strongest_cells list removed and the next item in the bank added at the tail
                     tmp_list = strongest_cells[:]
                     del tmp_list[strong_cell]
@@ -29,5 +29,5 @@ def get_largest_joltage(file_name: str):
             total_joltage = total_joltage + joltage
     return total_joltage
 
-joltage = get_largest_joltage("sample_input.txt")
+joltage = get_largest_joltage("sample_input.txt", 12)
 print(f'joltage: {joltage}')
